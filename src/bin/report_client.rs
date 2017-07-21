ec4d36c4307aaafa11048699c98aa34b3f217523
Sehyo Chang <sehyo@nginx.com>
2017-07-21 14:45:02 -0700

    made test client work with dummy data!

--- src/bin/report_client.rs
+++ src/bin/report_client.rs
@@ -10,20 +10,40 @@ extern crate libc;
 
 
 use std::env;
+use std::collections::HashMap;
 use mixer::service_grpc::MixerClient;
 use mixer::report::ReportRequest;
+use mixer::attributes::Attributes;
+use mixer::service_grpc::Mixer;
 
+static KEY1: i32 = 0;
 
 fn main() {
-    let client = MixerClient::new_plain("localhost", 50051, Default::default()).unwrap();
 
+    println!("creating mixer client at local host");
+
+    let client = MixerClient::new_plain("localhost", 9091, Default::default()).expect("init");
+
+
+    let mut requests = Vec::new();
     let mut req = ReportRequest::new();
-    // req.set_name(String::from("Hello, world!"));
+    let mut attr = Attributes::new();
+    //attr.set_string_attributes("")
+    req.set_request_index(0);
 
-    //let resp = client.check(grpc::RequestOptions::new(), req);
+    let mut attrValues: HashMap<i32,String> = HashMap::new();
+    attrValues.insert(KEY1,String::from("kuberentes://xyzdefault"));
+    attr.set_dictionary(attrValues);
 
-    //resp.wait();
+    req.set_attribute_update(attr);
 
 
-    println!("send report request");
+    requests.push(req);
+
+
+    let resp = client.report(grpc::RequestOptions::new(), grpc::StreamingRequest::iter(requests));
+    ;
+
+
+    println!("send report request: {} count",resp.wait_drop_metadata().count());
 }
