544869b3cb09cd42fa7ed6166e234274e0fd881a
Sehyo Chang <sehyo@nginx.com>
2017-07-27 21:26:27 -0700

    convert to filter module create proxy location in the nginx config for testing

--- ngx_http_istio_mixer_module.c
+++ ngx_http_istio_mixer_module.c
@@ -15,13 +15,25 @@
 #include <ngx_http.h>
 
 
-#define HELLO_WORLD "hello world"
+typedef struct {
+    ngx_hash_t                          types;
+    ngx_array_t                        *types_keys;
+    ngx_http_complex_value_t           *variable;
+} ngx_http_mixer_loc_conf_t;
+
 
 static char *ngx_http_istio_mixer(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
-static ngx_int_t ngx_http_istio_mixer_handler(ngx_http_request_t *r);
+static ngx_int_t ngx_http_istio_mixer_filter(ngx_http_request_t *r);
+static ngx_int_t ngx_http_mixer_filter_init(ngx_conf_t *cf);
+
+// create configuration
+static void *ngx_http_mixer_create_loc_conf(ngx_conf_t *cf);
+static char *ngx_http_mixer_merge_loc_conf(ngx_conf_t *cf, void *parent,
+    void *child);
 
 char  *mixer_client(ngx_http_request_t *r);
 
+static ngx_http_output_header_filter_pt ngx_http_next_header_filter;
 
 
 /**
@@ -41,13 +53,12 @@ static ngx_command_t ngx_http_istio_mixer_commands[] = {
     ngx_null_command /* command termination */
 };
 
-/* The hello world string. */
-static u_char ngx_hello_world[] = HELLO_WORLD;
+
 
 /* The module context. */
 static ngx_http_module_t ngx_http_istio_mixer_module_ctx = {
     NULL, /* preconfiguration */
-    NULL, /* postconfiguration */
+    ngx_http_mixer_filter_init, /* postconfiguration */
 
     NULL, /* create main configuration */
     NULL, /* init main configuration */
@@ -55,8 +66,8 @@ static ngx_http_module_t ngx_http_istio_mixer_module_ctx = {
     NULL, /* create server configuration */
     NULL, /* merge server configuration */
 
-    NULL, /* create location configuration */
-    NULL /* merge location configuration */
+    ngx_http_mixer_create_loc_conf, /* create location configuration */
+    ngx_http_mixer_merge_loc_conf /* merge location configuration */
 };
 
 /* Module definition. */
@@ -75,6 +86,16 @@ ngx_module_t ngx_http_istio_mixer_module = {
     NGX_MODULE_V1_PADDING
 };
 
+static ngx_int_t ngx_http_mixer_filter_init(ngx_conf_t *cf) {
+
+    
+    ngx_http_next_header_filter = ngx_http_top_header_filter;
+    ngx_http_top_header_filter = ngx_http_istio_mixer_filter;
+
+
+    return NGX_OK;   
+}
+
 /**
  * Content handler.
  *
@@ -83,70 +104,80 @@ ngx_module_t ngx_http_istio_mixer_module = {
  * @return
  *   The status of the response generation.
  */
-static ngx_int_t ngx_http_istio_mixer_handler(ngx_http_request_t *r)
+static ngx_int_t ngx_http_istio_mixer_filter(ngx_http_request_t *r)
 {
-    ngx_buf_t *b;
-    ngx_chain_t out;
 
-    ngx_log_error(NGX_LOG_ERR, ngx_cycle->log, 0, "start invoking istio mixer module");
+    ngx_log_error(NGX_LOG_ERR, ngx_cycle->log, 0, "start invoking istio mixer filter");
 
 
    // invoke mix client
-   char *result = mixer_client(r);
+   mixer_client(r);
 
-   ngx_log_error(NGX_LOG_ERR, ngx_cycle->log, 0, "finish calling istio module: %s",result);
-
-
+   ngx_log_error(NGX_LOG_ERR, ngx_cycle->log, 0, "finish calling istio filter");
 
 
-    // end of parsing header, just send output
+   return ngx_http_next_header_filter(r);
 
-    /* Set the Content-Type header. */
-    r->headers_out.content_type.len = sizeof("text/plain") - 1;
-    r->headers_out.content_type.data = (u_char *) "text/plain";
-
-    /* Allocate a new buffer for sending out the reply. */
-    b = ngx_pcalloc(r->pool, sizeof(ngx_buf_t));
-
-    /* Insertion in the buffer chain. */
-    out.buf = b;
-    out.next = NULL; /* just one buffer */
-
-    b->pos = ngx_hello_world; /* first position in memory of the data */
-    b->last = ngx_hello_world + sizeof(ngx_hello_world); /* last position in memory of the data */
-    b->memory = 1; /* content is in read-only memory */
-    b->last_buf = 1; /* there will be no more buffers in the request */
-
-    /* Sending the headers for the reply. */
-    r->headers_out.status = NGX_HTTP_OK; /* 200 status code */
-    /* Get the content length of the body. */
-    r->headers_out.content_length_n = sizeof(ngx_hello_world);
-    ngx_http_send_header(r); /* Send the headers */
-
-    /* Send the body, and return the status code of the output filter chain. */
-    return ngx_http_output_filter(r, &out);
-} /* ngx_http_hello_world_handler */
+} 
 
 
-/**
- * Configuration setup function that installs the content handler.
- *
- * @param cf
- *   Module configuration structure pointer.
- * @param cmd
- *   Module directives structure pointer.
- * @param conf
- *   Module configuration structure pointer.
- * @return string
- *   Status of the configuration setup.
- */
-static char *ngx_http_istio_mixer(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+static void *ngx_http_mixer_create_loc_conf(ngx_conf_t *cf) {
+
+    ngx_log_error(NGX_LOG_ERR, ngx_cycle->log, 0, "creating loc conf");
+
+    ngx_http_mixer_loc_conf_t  *conf;
+
+    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_mixer_loc_conf_t));
+    if (conf == NULL) {
+        return NULL;
+    }
+
+    /*
+     * set by ngx_pcalloc():
+     *
+     *     conf->types = { NULL };
+     *     conf->types_keys = NULL;
+     *     conf->variable = NULL;
+     */
+
+    return conf;
+}
+
+static char *
+ngx_http_mixer_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
 {
-    ngx_http_core_loc_conf_t *clcf; /* pointer to core location configuration */
+    ngx_log_error(NGX_LOG_ERR, ngx_cycle->log, 0, "merging loc conf");
 
-    /* Install the istio mixer  handler. */
-    clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
-    clcf->handler = ngx_http_istio_mixer_handler;
+    ngx_http_mixer_loc_conf_t  *prev = parent;
+    ngx_http_mixer_loc_conf_t  *conf = child;
+
+    if (ngx_http_merge_types(cf, &conf->types_keys, &conf->types,
+                             &prev->types_keys,&prev->types,
+                             ngx_http_html_default_types)
+        != NGX_OK)
+    {
+       return NGX_CONF_ERROR;
+    }
+
+    if (conf->variable == NULL) {
+        conf->variable = prev->variable;
+    }
+
+    if (conf->variable == NULL) {
+        conf->variable = (ngx_http_complex_value_t *) -1;
+    }
 
     return NGX_CONF_OK;
-} 
+}
+
+// init config
+// borrow from https://github.com/alibaba/nginx-http-footer-filter/blob/master/ngx_http_footer_filter_module.c
+// not sure what this code is doing...
+
+static char *ngx_http_istio_mixer(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+{
+   
+     ngx_log_error(NGX_LOG_ERR, ngx_cycle->log, 0, "configuring mixer");
+
+    return NGX_OK;
+}
